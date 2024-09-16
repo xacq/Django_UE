4f39b476e9b270555f4d11611fdef11a51d0a085
Green PC <83514730+xacq@users.noreply.github.com>
2024-09-15 21:08:59 -0500

    Add files via upload

---
+++ unidad_educativa/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for unidad_educativa project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'unidad_educativa.settings')
+
+application = get_asgi_application()
